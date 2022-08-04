b909b881b284a25ad349c0ec196650a9e77c49b1
OliviaMihalache <olivia.mihalache08@gmail.com>
2022-08-04 19:48:57 +0200

    customer, refund

---
+++ src/main/java/ro/sda/java37/finalProject/entities/Customer.java
@@ -0,0 +1,9 @@
+package ro.sda.java37.finalProject.entities;
+
+public class Customer {
+    public String firstName;
+    public String Name;
+    public String email;
+    public String address;
+
+}
---
+++ src/main/java/ro/sda/java37/finalProject/entities/Refund.java
@@ -0,0 +1,12 @@
+package ro.sda.java37.finalProject.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Refund {
+    public String employee;
+    public Date dateOfReturn;
+    public String reservation;
+    public BigDecimal surcharge;
+    public String comments;
+}
