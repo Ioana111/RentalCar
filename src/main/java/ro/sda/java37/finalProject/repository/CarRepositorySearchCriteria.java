4e74e787aa5a29bc048af53d673812988bb6387f
DESKTOP-G1OVDRU\marin <marinioana23@yahoo.com>
2022-08-09 21:42:27 +0300

    Added first controller

--- src/main/java/ro/sda/java37/finalProject/repository/CarRepositorySearchCriteria.java
+++ src/main/java/ro/sda/java37/finalProject/repository/CarRepositorySearchCriteria.java
@@ -2,6 +2,7 @@ package ro.sda.java37.finalProject.repository;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import ro.sda.java37.finalProject.entities.Car;
 
 import javax.persistence.EntityManager;
@@ -11,38 +12,38 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
-
+@Repository
 public class CarRepositorySearchCriteria {
     @Autowired
     private EntityManager em;
 
-    public List<Car>  findAllCars(ro.sda.java37.finalProject.DTO.CarDto search){
+    public List<Car>  findAllCars(ro.sda.java37.finalProject.dto.CarDto search){
         CriteriaBuilder builder=em.getCriteriaBuilder();
         CriteriaQuery<Car> query= builder.createQuery(Car.class);
         Root<Car> root= query.from(Car.class);
         List<Predicate> predicates = new ArrayList<>();
         if (StringUtils.isNotBlank(search.getBrand())){
-            Predicate hasBrand = builder.equal(root.get("Car_.brand"),search.getBrand());
+            Predicate hasBrand = builder.equal(root.get("car0_.brand"),search.getBrand());
             predicates.add(hasBrand);
         }
         if (StringUtils.isNotBlank(search.getModel())){
-            Predicate hasModel = builder.equal(root.get("Car_.model"),search.getModel());
+            Predicate hasModel = builder.equal(root.get("car0_.model"),search.getModel());
             predicates.add(hasModel);
         }
         if (StringUtils.isNotBlank(search.getBodyType())){
-            Predicate hasBodyType = builder.equal(root.get("Car_.bodyType"),search.getBodyType());
+            Predicate hasBodyType = builder.equal(root.get("car0_.bodyType"),search.getBodyType());
             predicates.add(hasBodyType);
         }
         if (StringUtils.isNotBlank(search.getColor())){
-            Predicate hasColor = builder.equal(root.get("Car_.color"),search.getColor());
+            Predicate hasColor = builder.equal(root.get("car0_.color"),search.getColor());
             predicates.add(hasColor);
         }
-        if (StringUtils.isNotBlank(Integer.toString(search.getYear()))){
-            Predicate hasYear  = builder.equal(root.get("Car_.year"),search.getYear());
+        if (search.getYear()!=null){
+            Predicate hasYear  = builder.equal(root.get("car0_.year"),search.getYear());
             predicates.add(hasYear);
         }
-        if (StringUtils.isNotBlank(Integer.toString(search.getMileage()))){
-            Predicate hasMileage  = builder.equal(root.get("Car_.mileage"),search.getMileage());
+        if (search.getMileage()!=null){
+            Predicate hasMileage  = builder.equal(root.get("car0_.mileage"),search.getMileage());
             predicates.add(hasMileage);
         }
 
--- src/main/java/ro/sda/java37/finalProject/services/CarService.java
+++ src/main/java/ro/sda/java37/finalProject/services/CarService.java
@@ -5,16 +5,29 @@ import org.springframework.stereotype.Service;
 import ro.sda.java37.finalProject.dto.CarDto;
 import ro.sda.java37.finalProject.entities.Car;
 import ro.sda.java37.finalProject.repository.CarRepository;
+import ro.sda.java37.finalProject.repository.CarRepositorySearchCriteria;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class CarService {
 CarMapper carMapper;
 CarRepository carRepository;
+CarRepositorySearchCriteria carRepositorySearchCriteria;
 
     public CarDto createCar(CarDto car) {
         Car carEntity= carMapper.convertToEntity(car);
         carRepository.save(carEntity);
         return carMapper.convertToDto(carEntity);
     }
+
+    public List<CarDto> listAllCars() {
+        return carRepository.findAll().stream().map(c-> carMapper.convertToDto(c)).collect(Collectors.toList());
+    }
+
+    public List<CarDto> search(CarDto search) {
+        return carRepositorySearchCriteria.findAllCars(search).stream().map(c->carMapper.convertToDto(c))
+                .collect(Collectors.toList());    }
 }
