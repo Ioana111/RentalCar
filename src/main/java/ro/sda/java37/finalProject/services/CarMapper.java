57ff7cb2de9ef4b8101dd78650650610a7e9b62f
raoul <raoul.rogojinaru@outlook.com>
2022-08-09 21:22:46 +0300

    CarMapper

--- src/main/java/ro/sda/java37/finalProject/services/CarMapper.java
+++ src/main/java/ro/sda/java37/finalProject/services/CarMapper.java
@@ -1,16 +1,37 @@
 package ro.sda.java37.finalProject.services;
 
-import ro.sda.java37.finalProject.dto.CarDto;
+import ro.sda.java37.finalProject.DTO.CarDto;
 import ro.sda.java37.finalProject.entities.Car;
 
 public class CarMapper implements Mapper<Car, CarDto> {
     @Override
     public CarDto convertToDto(Car entity) {
-        return null;
+        CarDto result = new CarDto();
+        result.setId(entity.getId());
+        result.setAvailable(entity.isAvailable());
+        result.setBodyType(entity.getBodyType());
+        result.setBrand(entity.getBrand());
+        result.setColor(entity.getColor());
+        result.setAmountPerDay(entity.getAmountPerDay());
+        result.setMileage(entity.getMileage());
+        result.setModel(entity.getModel());
+        result.setYear(entity.getYear());
+        return result;
     }
 
     @Override
     public Car convertToEntity(CarDto dto) {
-        return null;
+        Car result = new Car();
+        result.setId(dto.getId());
+        result.setAvailable(dto.isAvailable());
+        result.setBodyType(dto.getBodyType());
+        result.setBrand(dto.getBrand());
+        result.setColor(dto.getColor());
+        result.setAmountPerDay(dto.getAmountPerDay());
+        result.setMileage(dto.getMileage());
+        result.setModel(dto.getModel());
+        result.setYear(dto.getYear());
+        return result;
+
     }
 }
